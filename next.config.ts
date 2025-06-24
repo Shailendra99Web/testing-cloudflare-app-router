af0443c6fc4ecccd569780ed2952d1186e4a551e
Shailendra Singh Verma <shailendra99web@gmail.com>
2025-06-24 19:29:23 +0530

    second

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // ✅ Enables static export
 };
 
 export default nextConfig;
